dc1f87af024428686389f81af29cc8930c933aeb
arnav404 <arnav@Arnav.local>
2021-06-23 14:34:54 -0500

    added avgs

--- app.py
+++ app.py
@@ -13,23 +13,53 @@ player = "James Harden"
 year = "2021"
 stat = "pts"
 
-url2 = "https://www.basketball-reference.com/leagues/NBA_" + year + "_per_game.html"
-
-client2 = uReq(url2)
-totals_html = client2.read()
-client2.close()
-
-soup2 = soup(totals_html, "html.parser")
-
-totals = soup2.findAll("table", {"id": "per_game_stats"})[0].tbody.findAll(
-    "tr", {"class": "full_table"}
-)
+avgs = [0, 0, 0]
 
 
 @app.route("/")
 @cross_origin()
-def getAverage():
+def helloWorld():
     return json.dumps({"data": "Hello World"})
+
+
+@app.route("/getavgs/<pl>", methods=["POST", "GET"])
+@cross_origin()
+def getAverages(pl):
+    player = pl
+
+    # player string url
+    player_input = (
+        player.split(" ")[1][0:5].lower() + player.split(" ")[0][0:2].lower() + "01"
+    )
+
+    # url of bball reference page
+    my_url = (
+        "https://www.basketball-reference.com/players/c/"
+        + player_input
+        + "/gamelog/"
+        + year
+        + "#pgl_basic"
+    )
+
+    # getting the data
+    uClient = uReq(my_url)
+    page_html = uClient.read()
+    uClient.close()
+    page_soup = soup(page_html, "html.parser")
+
+    data = page_soup.find("div", {"class": "p1"}).findAll("div")
+
+    print(data)
+
+    for i in range(len(data)):
+        if data[i].find("h4")["data-tip"] == "Points":
+            avgs[0] = data[i].find("p").text
+        if data[i].find("h4")["data-tip"] == "Total Rebounds":
+            avgs[1] = data[i].find("p").text
+        if data[i].find("h4")["data-tip"] == "Assists":
+            avgs[2] = data[i].find("p").text
+
+    return json.dumps({"data": avgs})
 
 
 @app.route("/getlogs/<pl>/<st>", methods=["POST", "GET"])
